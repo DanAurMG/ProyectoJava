124dc22858271ce747c052d79c4b87eeac57baca
DanAurMG <raizeremi.15.damg@gmail.com>
2021-11-17 08:58:49 -0600

    Comentando codigo

--- Aeronave/Aeronave.java
+++ Aeronave/Aeronave.java
@@ -4,31 +4,38 @@ public class Aeronave{
     private int altitud;
     private int tripulacion;
     private String nombre; 
+    //Constructor Aeronave
     public Aeronave(int altitud, int tripulacion, String nombre){
         this.setAltitud(altitud);
         this.setTripulacion(tripulacion);
         this.setNombre(nombre);
     }
-    public int getTripulacion() {
-        return tripulacion;
-    }
-    public void setTripulacion(int tripulacion) {
-        this.tripulacion = tripulacion;
-    }
-    public int getAltitud() {
-        return altitud;
-    }
-    public void setAltitud(int altitud) {
-        this.altitud = altitud;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
     public void Volar(){
         
+    }
+
+    public int getTripulacion() {
+        return tripulacion;
+    }
+
+    public void setTripulacion(int tripulacion) {
+        this.tripulacion = tripulacion;
+    }
+
+    public int getAltitud() {
+        return altitud;
+    }
+
+    public void setAltitud(int altitud) {
+        this.altitud = altitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
